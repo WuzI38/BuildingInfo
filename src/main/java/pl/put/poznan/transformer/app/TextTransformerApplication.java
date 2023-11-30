4b55c560221e3924ee9d4ba8df72791698e9b75c
wylupek <filip@tapoczta.pl>
2023-11-30 18:32:16 +0100

    Added name param in Adder.calculate

--- src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
+++ src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
@@ -1,38 +1,17 @@
 package pl.put.poznan.transformer.app;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import pl.put.poznan.transformer.logic.TextTransformer;
-import pl.put.poznan.transformer.logic.Location;
-import pl.put.poznan.transformer.logic.JSONHandle;
+
 
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
 public class TextTransformerApplication {
 
     public static void main(String[] args) {
         TextTransformer tt = new TextTransformer();
-        Location loc1 = tt.generateLocation(tt.getText("src/main/resources/testGood.json"));
-        System.out.println(loc1.getName());
+        System.out.println(tt.getParam("src/main/resources/testGood.json", "Podlokacja B2", "area"));
 
-
-
-        try {
-            System.out.println(JSONHandle.prettyPrintJson(tt.getText("src/main/resources/testGood.json")));
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-
-
-        //         SpringApplication.run(TextTransformerApplication.class, args);
+        // SpringApplication.run(TextTransformerApplication.class, args);
     }
 }
-
-/*
-
-"name": "Podlokacja A1",
-          "area": 20.0,
-          "cube": 15.2,
-          "heating": 10.0,
-          "light":
- */
