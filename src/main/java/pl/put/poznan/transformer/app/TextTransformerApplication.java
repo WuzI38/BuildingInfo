2eeeda4bf268718fe8fabedea7f7bc4ee070f799
wylupek <filip@tapoczta.pl>
2023-11-30 16:39:41 +0100

    Refactor into Validate and JSONHandler

--- src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
+++ src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
@@ -1,18 +1,30 @@
 package pl.put.poznan.transformer.app;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import pl.put.poznan.transformer.logic.TextTransformer;
-
+import pl.put.poznan.transformer.logic.Location;
+import pl.put.poznan.transformer.logic.JSONHandle;
 
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
 public class TextTransformerApplication {
 
     public static void main(String[] args) {
         TextTransformer tt = new TextTransformer();
-        tt.getParam("src/main/resources/testGood.json", "Budynek", "light");
+        Location loc1 = tt.generateLocation(tt.getText("src/main/resources/testGood.json"));
+        System.out.println(loc1.getName());
 
-        // SpringApplication.run(TextTransformerApplication.class, args);
+
+
+        try {
+            System.out.println(JSONHandle.prettyPrintJson(tt.getText("src/main/resources/testGood.json")));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        //         SpringApplication.run(TextTransformerApplication.class, args);
     }
 }
 
